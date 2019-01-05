bbcf4dea798ed6e7049688e4129b4ae7d46d8f44
Chris Arsenault <carsena2@gmail.com>
2019-01-05 04:45:31 -0500

    add-build-script

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class BuildScript
+{
+    [MenuItem("Build/Build WebGL")]
+    static void PerformBuild()
+    {
+        var sceneSettings = EditorBuildSettings.scenes;
+        string[] scenePaths = new string[sceneSettings.Length];
+
+        for (int i = 0; i < scenePaths.Length; ++i)
+        {
+            scenePaths[i] = sceneSettings[i].path;
+        }
+        BuildPipeline.BuildPlayer(scenePaths, "build/web-gl", BuildTarget.WebGL, BuildOptions.None);
+    }
+}
