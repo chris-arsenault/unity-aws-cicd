d72c90052b1c1ac297e883e156c878e59b04ee4e
Chris Arsenault <carsena2@gmail.com>
2019-01-05 05:13:27 -0500

    add debug build

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using System;
 
 public class BuildScript
 {
@@ -8,10 +9,14 @@ public class BuildScript
     static void PerformBuild()
     {
         var sceneSettings = EditorBuildSettings.scenes;
+        Console.WriteLine(sceneSettings);
         string[] scenePaths = new string[sceneSettings.Length];
 
+        Console.WriteLine(sceneSettings.Length);
         for (int i = 0; i < scenePaths.Length; ++i)
         {
+            Console.WriteLine(sceneSettings[i].guid);
+            Console.WriteLine(sceneSettings[i].path);
             scenePaths[i] = sceneSettings[i].path;
         }
         BuildPipeline.BuildPlayer(scenePaths, "build/web-gl", BuildTarget.WebGL, BuildOptions.None);
